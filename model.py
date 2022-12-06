88e541934452780106c744cfdd34df053fa889ba
Aaryan Shah <73857150+aaryans1@users.noreply.github.com>
2022-12-06 16:46:02 -0500

    Added starter files

---
+++ model.py
@@ -0,0 +1,19 @@
+import unittest
+import sqlite3
+import json
+import os
+import matplotlib.pyplot as plt
+
+def read_data(filename):
+    full_path = os.path.join(os.path.dirname(__file__), filename)
+    f = open(full_path)
+    file_data = f.read()
+    f.close()
+    json_data = json.loads(file_data)
+    return json_data
+
+def open_database(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
