4629575fa601cd3b1deb718a682e14d36ab1ae42
Isha Mendonza <ishamendonza@Ishas-MacBook-Air-5.local>
2022-12-07 16:59:48 -0500

    url list

--- scraper.py
+++ scraper.py
@@ -12,7 +12,9 @@ def main():
     soup = BeautifulSoup(r.text, 'html.parser')
     # Call the functions getLink(soup) and getAdmissionsInfo2019(soup) on your soup object.
     
+    endings_list = ['neil-young-powderfinger-1224887/', 'david-bowie-station-to-station-3-1224938/', 'john-prine-angel-from-montgomery-1224988/', 'the-b-52s-rock-lobster-2-1225038/', 'jimi-hendrix-purple-haze-2-1225088/', 'david-bowie-changes-2-1225138/', 'green-day-basket-case-1225188/', 'bob-dylan-blowin-in-the-wind-3-1225238/', 'daddy-yankee-feat-glory-gasolina-1225288/']
 
 
+    
 if __name__ == "__main__":
     main()
