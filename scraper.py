802dcef6689807ad4c66d7a2ed31cd1a2facdb41
Isha Mendonza <ishamendonza@Ishas-MacBook-Air-5.local>
2022-12-07 18:44:28 -0500

    looping through the pages

--- scraper.py
+++ scraper.py
@@ -14,7 +14,9 @@ def main():
     
     endings_list = ['neil-young-powderfinger-1224887/', 'david-bowie-station-to-station-3-1224938/', 'john-prine-angel-from-montgomery-1224988/', 'the-b-52s-rock-lobster-2-1225038/', 'jimi-hendrix-purple-haze-2-1225088/', 'david-bowie-changes-2-1225138/', 'green-day-basket-case-1225188/', 'bob-dylan-blowin-in-the-wind-3-1225238/', 'daddy-yankee-feat-glory-gasolina-1225288/']
 
+    for i in range(len(endings_list)): 
+        url = 'https://www.rollingstone.com/music/music-lists/best-songs-of-all-time-1224767/' + endings_list[i]
+    #class c-gallery-vertical-album__title
 
-    
 if __name__ == "__main__":
     main()
